2bcf34584424d01738d967a910fe7468de9a2400
Onur Caglayan <onurcag99@gmail.com>
2022-10-10 22:27:12 +0200

    asd

--- pi/pi/main.cpp
+++ pi/pi/main.cpp
@@ -7,23 +7,23 @@
 
 int main()
 {
-	//omp_set_nested(1);
-	//int threads = omp_get_max_threads();
-	//size_t digits = 1000000;
-	//std::vector<std::vector<my_complex>> twiddle_table;
+	omp_set_nested(1);
+	int threads = omp_get_max_threads();
+	size_t digits = 1000;
+	std::vector<std::vector<my_complex>> twiddle_table;
 
-	////  Figure out how large to make the table:
-	////  Determine minimum FFT size.
-	//size_t p = 2 * digits / 9 + 10;
-	//int k = 0;
-	//size_t length = 1;
-	//while (length < 3 * p) {
-	//	length <<= 1;
-	//	k++;
-	//}
-	//fft_ensure_table(twiddle_table, k);
+	//  Figure out how large to make the table:
+	//  Determine minimum FFT size.
+	size_t p = 2 * digits / 9 + 10;
+	int k = 0;
+	size_t length = 1;
+	while (length < 3 * p) {
+		length <<= 1;
+		k++;
+	}
+	fft_ensure_table(twiddle_table, k);
 
-	//dump_to_file("pi_output.txt", Pi(twiddle_table, digits, threads, false));
+	dump_to_file("pi_output.txt", Pi(twiddle_table, digits, threads, false));
 
-	std::cout << std::to_string(find_pattern("3.141568424984325", "9999999999999999999999"));
+	//std::cout << std::to_string(find_pattern("3.141568424984325", "9999999999999999999999"));
 }
