35da7497e7c46cdd42486bc6a0b35ece00c96fdc
Onur Caglayan <onurcag99@gmail.com>
2022-11-25 02:17:54 +0100

    DELETE PROJECT

--- pi/pi/helpers.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-double wall_clock();
-void dump_to_file(const char* path, const std::string& str);
--- pi/pi/pi.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void Pi_BSR(std::vector<std::vector<my_complex>>& twiddle_table, BigFloat& P, BigFloat& Q, BigFloat& R, uint32_t a, uint32_t b, size_t p, int tds = 1);
-std::string Pi(std::vector<std::vector<my_complex>>& twiddle_table, size_t digits, int tds, bool silent_mode);
